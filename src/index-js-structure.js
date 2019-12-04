4787d97f9b27d13f36d8c65c5e112d9c4d3dc042
iulianstave <iulian.staverescu@gmail.com>
2019-12-04 13:59:39 +0200

    Add SeasonDisplay child component

---
+++ src/index-js-structure.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+  /*
+  class App extends React.Component {
+    state = { lat: null, errorMessage: "" };
+  
+    componentDidMount() 
+    render() {}
+      return <div>Loading...</div>;
+  }
+  ReactDOM.render(<App />, document.getElementById("root"));componentDidMount() {
+  */
+
+ class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    //call the geolocator API service
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    return <div>Loading...</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  state = { lat: null, errorMessage: "" };
+ state = { lat: null, errorMessage: '' };
 
-  componentDidMount() {
-    //call the geolocator API service
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
+ componentDidMount() {
+  //call the geolocator API service
+  window.navigator.geolocation.getCurrentPosition(
+    position => this.setState({ lat: position.coords.latitude }),
+    err => this.setState({ errorMessage: err.message })
+  );
+}
 
-  render() {
-    if (this.state.lat && !this.state.errorMessage) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }
-    if (!this.state.lat && this.state.errorMessage) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-    return <div>Loading...</div>;
+render() {
+  if (this.state.lat && !this.state.errorMessage) {
+    return <div>Latitude: {this.state.lat}</div>;
   }
+  if (!this.state.lat && this.state.errorMessage) {
+    return <div>Error: {this.state.errorMessage}</div>;
+  }
+  return <div>Loading...</div>;
+}
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
