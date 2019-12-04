0045648172560f546d52357b9693310d740a5076
iulianstave <iulian.staverescu@gmail.com>
2019-12-04 16:03:40 +0200

    Refactor with config

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,28 +1,40 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: "Summer time",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it's cold outside",
+    iconName: 'snowflake'
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer':'winter';
   } else {
     return lat > 0 ? 'winter': 'summer';
   }
-}
+};
 
 
 const SeasonDisplay = (props) => {
   const month = new Date().getMonth();
   const season = getSeason(props.lat, month);
+  const { text, iconName } = seasonConfig[season];
+  /*
   const seasonText = 
     season === 'winter' ? 'Blurr, it is chilly' : 'Summer time';
   const icon =
     season === 'winter' ? 'snowflake' : 'sun';
-
+  */
   return (
     <div>
-      <i className = "blind icon" />
-      <i className = {`${icon} icon`} />
-      <h1>{seasonText}</h1>
-      <i className = {`${icon} icon`} />
+      <i className = {`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className = {`${iconName} icon`} />
     </div>
   );
 };
